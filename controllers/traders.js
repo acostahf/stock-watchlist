cbc5153dd65db261493c8c8837172d00ce46bdce
Fabian Acosta <acosahf@hotmail.com>
2019-12-08 23:03:20 -0600

    made the update function to work in the show ejs

--- controllers/traders.js
+++ controllers/traders.js
@@ -5,13 +5,11 @@ module.exports = {
   addStock,
   delStock,
   show,
-  // update,
+  update
   // edit
-
 };
 
 function index(req, res, next) {
-
   // Make the query object to use with Student.find based up
   // the user has submitted the search form or now
   let modelQuery = req.query.name
@@ -21,7 +19,7 @@ function index(req, res, next) {
   let sortKey = req.query.sort || "name";
   Trader.find(modelQuery)
     .sort(sortKey)
-    .exec(function (err, traders) {
+    .exec(function(err, traders) {
       if (err) return next(err);
       // Passing search values, name & sortKey, for use in the EJS
       res.render("traders/index", {
@@ -35,40 +33,38 @@ function index(req, res, next) {
 
 function addStock(req, res, next) {
   for (let key in req.body) {
-    if (req.body[key] === '') delete req.body[key];
+    if (req.body[key] === "") delete req.body[key];
   }
   req.user.stocks.push(req.body);
-  req.user.save(function (err) {
+  req.user.save(function(err) {
     res.redirect("/traders");
   });
 }
 function delStock(req, res, next) {
-  Trader.findOne({ 'stocks._id': req.params.id }, function (err, trader) {
+  Trader.findOne({ "stocks._id": req.params.id }, function(err, trader) {
     trader.stocks.id(req.params.id).remove();
-    trader.save(function (err) {
+    trader.save(function(err) {
       res.redirect("/traders");
-
     });
   });
 }
 
 function show(req, res) {
-  Trader.findOne({ _id: req.user._id })
-    .exec((err, trader) => {
-      let stocks = trader.stocks;
-      stocks.forEach(function (h) {
-        let correctStock = "";
-        if (h._id == req.params.id) {
-          correctStock = h;
-          res.render("traders/show", {
-            trader,
-            correctStock,
-            user: req.user,
-            name: req.query.name
-          });
-        }
-      });
+  Trader.findOne({ _id: req.user._id }).exec((err, trader) => {
+    let stocks = trader.stocks;
+    stocks.forEach(function(h) {
+      let correctStock = "";
+      if (h._id == req.params.id) {
+        correctStock = h;
+        res.render("traders/show", {
+          trader,
+          correctStock,
+          user: req.user,
+          name: req.query.name
+        });
+      }
     });
+  });
   // });
 }
 // function edit(req, res, next) {
@@ -79,11 +75,24 @@ function show(req, res) {
 //     })
 //   })
 // }
-// function update(req, res, next) {
-//   let traderStock = Trader.stocks
-//   traderStock.findById(req.params.id, function (err, stoc) {
-//     res.redirect('/traders/show', {
 
-//     })
-//   })
-// }
+//this function updates by getting the trader id and looping through the many stocks the user
+//has to get the correctStock and req.body the input
+//to save to the stocks
+function update(req, res, next) {
+  Trader.findOne({ "stocks._id": req.params.id }, function(err, trader) {
+    trader.stocks.id(req.params.id);
+    let multStock = trader.stocks;
+    multStock.forEach(function(correctStock) {
+      if (correctStock._id == req.params.id) {
+        correctStock.symbol = req.body.symbol;
+        correctStock.price = req.body.price;
+        correctStock.buy = req.body.buy;
+        correctStock.sell = req.body.sell;
+        trader.save(function(err) {
+          res.redirect("/traders");
+        });
+      }
+    });
+  });
+}
--- routes/traders.js
+++ routes/traders.js
@@ -3,7 +3,7 @@ var tradersCtrl = require("../controllers/traders");
 
 // GET /traders
 router.get("/traders", tradersCtrl.index);
-router.get('/traders/:id/stocks', isLoggedIn, tradersCtrl.show);
+router.get("/traders/:id/stocks", isLoggedIn, tradersCtrl.show);
 // router.get('/traders/:id/edit', isLoggedIn, tradersCtrl.edit);
 
 //Post stocks
@@ -11,8 +11,7 @@ router.post("/stocks", isLoggedIn, tradersCtrl.addStock);
 router.delete("/stocks/:id", isLoggedIn, tradersCtrl.delStock);
 
 //PUT stock
-// router.put('/traders/:id/stocks', isLoggedIn, tradersCtrl.update)
-
+router.put("/traders/:id/stocks", isLoggedIn, tradersCtrl.update);
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
